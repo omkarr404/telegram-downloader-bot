b660791a43f83caa5b0fef521514f043a00cbd0b
Omkar Sawant <143311302+omkarr404@users.noreply.github.com>
2025-11-16 10:36:28 +0530

    Update format_handler.py

--- handlers/format_handler.py
+++ handlers/format_handler.py
@@ -1,17 +1,16 @@
-from telegram.ext import ContextTypes
 from utils.downloader import download_media
-from bot import ConversationHandler
+from telegram.ext import ConversationHandler
 
-async def choose_format(update, context: ContextTypes.DEFAULT_TYPE):
+async def format_handler(update, context):
     quality = update.message.text
-    link = context.user_data.get("link")
+    url = context.user_data["link"]
 
-    await update.message.reply_text("⬇ Downloading... please wait.")
+    await update.message.reply_text("⬇ Downloading... Please wait...")
 
     try:
-        file_path = await download_media(link, quality)
-        await update.message.reply_video(video=open(file_path, "rb"))
+        file_path = await download_media(url, quality)
+        await update.message.reply_video(open(file_path, "rb"))
     except Exception as e:
-        await update.message.reply_text(f"❌ Error: {str(e)}")
+        await update.message.reply_text(str(e))
 
     return ConversationHandler.END
